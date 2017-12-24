6332e3ff217763abbe24c584654fcd713868b34c
Bill Yu <han.yu@duke.edu>
2017-12-24 09:17:04 +0800

    configs

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es6": true
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module",
+    "allowImportExportEverywhere": true,
+    "codeFrame": false
+  },
+  "extends": "google"
+};
